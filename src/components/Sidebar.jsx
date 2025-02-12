4a9c08d8e6f4df0c502439f6a4b2f6b71ca2b5b5
Nisarg08z <nisarg693z@gmail.com>
2025-02-12 18:16:23 +0530

    day-12.2

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,36 +3,39 @@ import { Link } from "react-router-dom";
 
 const Sidebar = ({ hideSidebar }) => {
   const menuItems = [
-    { name: "Home", icon: "home.png" },
-    { name: "Liked Videos", icon: "liked.png" },
-    { name: "History", icon: "history.png" },
-    { name: "My Content", icon: "content.png" },
-    { name: "Collection", icon: "collection.png" },
-    { name: "Subscribers", icon: "subscribers.png" },
+    { name: "Home", icon: "home.png", path: "/" },
+    { name: "Liked Videos", icon: "liked.png", path: "/like/Videos" },
+    { name: "History", icon: "history.png", path: "/" },
+    { name: "My Content", icon: "content.png", path: "/" },
+    { name: "Collection", icon: "collection.png", path: "/" },
+    { name: "Subscribers", icon: "subscribers.png", path: "/" },
   ];
 
   const bottomMenuItems = [
-    { name: "Support", icon: "support.png" },
-    { name: "Settings", icon: "settings.png" },
+    { name: "Support", icon: "support.png", path: "/" },
+    { name: "Settings", icon: "settings.png", path: "/" },
   ];
 
   return (
     <aside
-      className={`bg-black border-r border-gray-700 flex flex-col justify-between h-screen p-4 transition-all duration-300  ${hideSidebar ? "w-20" : "w-64"
-        }`}
+      className={`bg-black border-r border-gray-700 flex flex-col justify-between h-screen p-4 transition-all duration-300 ${
+        hideSidebar ? "w-20" : "w-64"
+      }`}
     >
       <div>
-        <div className= "flex items-center mb-6 justify-center" >
+        <div className="flex items-center mb-6 justify-center">
           <Link to="/">
             <img src="/assets/logos/logo.png" alt="Logo" className="h-10" />
           </Link>
         </div>
         <nav>
           {menuItems.map((item) => (
-            <button
+            <Link
               key={item.name}
-              className={`w-full flex items-center text-gray-300 hover:text-white py-2 px-3 rounded hover:bg-gray-800  ${hideSidebar ? "justify-center" : ""
-                }`}
+              to={item.path}
+              className={`w-full flex items-center text-gray-300 hover:text-white py-2 px-3 rounded hover:bg-gray-800 ${
+                hideSidebar ? "justify-center" : ""
+              }`}
             >
               <img
                 src={`../assets/icons/${item.icon}`}
@@ -40,17 +43,19 @@ const Sidebar = ({ hideSidebar }) => {
                 className="h-6"
               />
               {!hideSidebar && <span className="ml-3">{item.name}</span>}
-            </button>
+            </Link>
           ))}
         </nav>
       </div>
 
       <div>
         {bottomMenuItems.map((item) => (
-          <button
+          <Link
             key={item.name}
-            className={`w-full flex items-center text-gray-300 hover:text-white py-2 px-3 rounded hover:bg-gray-800 ${hideSidebar ? "justify-center" : ""
-              }`}
+            to={item.path}
+            className={`w-full flex items-center text-gray-300 hover:text-white py-2 px-3 rounded hover:bg-gray-800 ${
+              hideSidebar ? "justify-center" : ""
+            }`}
           >
             <img
               src={`../assets/icons/${item.icon}`}
@@ -58,7 +63,7 @@ const Sidebar = ({ hideSidebar }) => {
               className="h-6"
             />
             {!hideSidebar && <span className="ml-3">{item.name}</span>}
-          </button>
+          </Link>
         ))}
       </div>
     </aside>
