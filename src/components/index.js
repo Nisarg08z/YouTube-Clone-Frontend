282e809f1e24ebc709230c753d878e412ef3e7ab
Nisarg08z <nisarg693z@gmail.com>
2025-03-07 15:53:40 +0530

    day-21.2

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Sidebar from "./Sidebar.jsx"
 import Header from "./Header.jsx"
 import { EmptyHomePage, EmptyPlayListPage, EmptyLikePage, EmptyHistoryPage, EmptyFollowPage } from "./EmptysState/"
 import { VideoGrid, VideoCard, VideoDetails, CommentSection, VideoPlayer } from "./VideoCard/"
-import { FollowingList, Playlist, TweetsList, VideosList }from "./ProfileCompo/index.js"
+import { FollowingList, Playlist, TweetsList, VideosList } from "./ProfileCompo";
 import { SubscriberCard, SubscriberGrid } from "./Subscriber"
 import { StatCard, VideoTable, VideoTableGrid, VideoUpload } from "./Content"
 import { UpdateDetail, UpdatePassword } from "./SettingCard"
